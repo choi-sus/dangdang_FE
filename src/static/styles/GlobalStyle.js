425280652d14a405c88a229fdc014cdb52961d4c
choi-sus <choi-sus@naver.com>
2022-03-26 05:24:28 +0900

    globalstyle 확인 중

--- src/static/styles/GlobalStyle.js
+++ src/static/styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset};
+  
   * {
     margin: 0;
     padding: 0;
