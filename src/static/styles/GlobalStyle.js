d214ea8ef4cc72d78a87303eb949808dc4af3746
choi-sus <choi-sus@naver.com>
2022-03-26 05:49:04 +0900

    font 수정 중

--- src/static/styles/GlobalStyle.js
+++ src/static/styles/GlobalStyle.js
@@ -1,12 +1,13 @@
 import { createGlobalStyle } from "styled-components";
-import reset from 'styled-reset';
+import reset from "styled-reset";
+import "../fonts/font.css"
 
 const GlobalStyle = createGlobalStyle`
-  
+  ${reset};
   * {
     margin: 0;
     padding: 0;
-    font-family: "Godo", NotoSansKR, sans-serif;
+    font-family: "Godo", "NotoSansKR", sans-serif !important;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
