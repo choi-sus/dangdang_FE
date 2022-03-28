5306e101f596f9b68244c976a4cf37ad52fedc0f
Jeong Hye in <cukimonster725@naver.com>
2022-03-28 17:16:18 +0900

    refresh token kakao test 2

--- src/shared/kakao.js
+++ src/shared/kakao.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {actionCreators as userActions} from "../redux/modules/user";
+import {history} from "../redux/configStore"
 
 const Kakao = () => {
   const dispatch = useDispatch();
   let authorization_code = new URL(window.location.href).searchParams.get("code");
   useEffect(() => {
     dispatch(userActions.kakaoLoginDB(authorization_code))
+    history.replace("/main");
   }, []); 
   return (
     <React.Fragment></React.Fragment>
